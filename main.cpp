b3ff316457c7bc627f8302e38ec98f4ad883954d
Avanti-k <avanti.199@gmail.com>
2019-11-22 20:19:32 -0500

    Prints after executed days incremented and deletes after print

--- main.cpp
+++ main.cpp
@@ -70,91 +70,130 @@ int execute_buildings()
 
     ofstream debug_op;
     debug_op.open("../project_material/sample_op2/op2_d.txt");
-    bool execution_on = false;
+    bool execution_on = false, building_finished = false;
     RBNode * curr;
     int days_remaining = 0, target = 0, start_time = 0, stop_time = 0;
     while(tot_bldg_cnt != completed_bldg_cnt || (!cmd_que.empty()))
     {
-     // check whether any command ready at this point
-     cout<<"\n ------------------------ Entered time : "<<timer<<"--------------------------"<<endl;
+        // check whether any command ready at this point
+        cout<<"\n ------------------------ Entered time : "<<timer<<"--------------------------"<<endl;
         cout<<"\n cmd que.front arrival time = "<<cmd_que.front().arrival_time<<endl;
-        if((timer != 0) && (execution_on == true))
+        /*if((timer != 0) && (execution_on == true))
         {
             curr->get_building()->executed_time += 1;
-        }
+        }*/
 
-        if(cmd_que.front().arrival_time == timer)
-     {
-        // cmds in sorted order, so just check first command.
-        // arrival time matches execute it
-        if(cmd_que.front().cmd_type == INSERT)
+
+        if(timer != 0)
         {
-            insert_new_building();
-            //completed_bldg_cnt ++;
-            //cout<<"\n************ inserted a building ******"<<endl;
-        }
-        else{
-            // command type in print
-            print_building_wrapper();
-        }
-        cmd_que.pop();// remove it from command que as it is served
-
-     }
-if(timer!= 0)
-{
-    if(!execution_on)
-    {
-
-        // pick up a min building and start execution
-
-        curr = MH.remove_min();
-        if(curr == NULL)
-        {
-            cout<<"MIN HEAP IS EMPTY\n"<<endl;
-            debug_op<<timer<<endl;
-
-            timer ++;
-            continue;
-        }
-        //cout<<"Buildin selected for execution = "<<curr->get_building()->get_building_num()<<endl;
-        execution_on = true;
-        start_time = timer;
-        days_remaining = curr->get_building()->get_total_time() - curr->get_building()->executed_time;
-        target = (days_remaining < MAX_EXEC_DAYS)? days_remaining: MAX_EXEC_DAYS;
-        stop_time = start_time + target - 1;
-        //cout<<"Start time = "<<start_time<<" Stop time = "<<stop_time<<endl;
-        curr->get_building()->executed_time += 1;
-
-    }
-     else{
-        // execuiton on
-        //check whether to stpo execution
-        if(timer == stop_time)
-        {
-            // stop time
-            // update executed time
-            //curr->get_building()->executed_time += target;
-            //curr->get_building()->executed_time += 1;
-
-            if(curr->get_building()->get_executed_time() == curr->get_building()->get_total_time())
+            if(!execution_on)
             {
-                // execution for the buildng complete remove it from RBT
-                cout<<"\n"<<timer<<": _______________ Completed buildin _______________"<<endl;
-                cout<<"("<<curr->get_building()->get_building_num()<<","<<timer<<")"<<endl;
-                cout<<"\n___________________________________________________"<<endl;
-                cout<<"_____________________________________________________\n\n"<<endl;
-                op<<"\n"<<timer<<" : ("<<curr->get_building()->get_building_num()<<","<<timer<<")"<<endl;
-                RBT.deleteNode(curr);
-                completed_bldg_cnt ++;
+
+                // pick up a min building and start execution
+
+                curr = MH.remove_min();
+                if(curr == NULL)
+                {
+                    cout<<"MIN HEAP IS EMPTY\n"<<endl;
+                    debug_op<<timer<<endl;
+
+                    timer ++;
+                    continue;
+                }
+                //cout<<"Buildin selected for execution = "<<curr->get_building()->get_building_num()<<endl;
+                execution_on = true;
+                start_time = timer;
+                days_remaining = curr->get_building()->get_total_time() - curr->get_building()->executed_time;
+                target = (days_remaining < MAX_EXEC_DAYS)? days_remaining: MAX_EXEC_DAYS;
+                stop_time = start_time + target - 1;
+                //cout<<"Start time = "<<start_time<<" Stop time = "<<stop_time<<endl;
+                curr->get_building()->executed_time += 1;
+
+            }
+            else
+            {
+                // execuiton on
+                //check whether to stpo execution
+                if(timer == stop_time)
+                {
+                    // stop time
+                    // update executed time
+                    //curr->get_building()->executed_time += target;
+                    curr->get_building()->executed_time += 1;
+
+                    if(curr->get_building()->get_executed_time() == curr->get_building()->get_total_time())
+                    {
+                        // execution for the buildng complete remove it from RBT
+                        cout<<"\n"<<timer<<": _______________ Completed buildin _______________"<<endl;
+                        cout<<"("<<curr->get_building()->get_building_num()<<","<<timer<<")"<<endl;
+                        cout<<"\n___________________________________________________"<<endl;
+                        cout<<"_____________________________________________________\n\n"<<endl;
+                        building_finished = true;
+                        /*
+                        op<<"\n"<<timer<<" : ("<<curr->get_building()->get_building_num()<<","<<timer<<")"<<endl;
+                        RBT.deleteNode(curr);
+                        completed_bldg_cnt ++;
+                        */
+                    }
+                    else
+                    {
+                        // execution still remaining so put it back in min heap
+                        // with updated executed time
+                        MH.insert_new(curr);
+                        cout<<"Updated exec time of building "<<curr->get_building()->get_building_num()
+                           <<"= "<<curr->get_building()->executed_time<<endl;
+                            // clearing up variales for next instance
+                    execution_on = false;
+                    start_time = 0;
+                    curr = NULL;
+                    days_remaining = 0;
+                    target = 0;
+                    stop_time = 0;
+                    }
+                    /*
+                    // clearing up variales for next instance
+                    execution_on = false;
+                    start_time = 0;
+                    curr = NULL;
+                    days_remaining = 0;
+                    target = 0;
+                    stop_time = 0;
+                    */
+
+                }
+                else
+                {
+                    //cout<<"ongoing"<<endl;
+                    curr->get_building()->executed_time += 1;
+                }
+
+            }
+        }
+
+        // SERVING COMMANDS
+        if(cmd_que.front().arrival_time == timer)
+        {
+            // cmds in sorted order, so just check first command.
+            // arrival time matches execute it
+            if(cmd_que.front().cmd_type == INSERT)
+            {
+                insert_new_building();
+                //completed_bldg_cnt ++;
+                //cout<<"\n************ inserted a building ******"<<endl;
             }
             else{
-                // execution still remaining so put it back in min heap
-                // with updated executed time
-                MH.insert_new(curr);
-                cout<<"Updated exec time of building "<<curr->get_building()->get_building_num()
-                   <<"= "<<curr->get_building()->executed_time<<endl;
+                // command type in print
+                print_building_wrapper();
             }
+            cmd_que.pop();// remove it from command que as it is served
+        }
 
+        if(building_finished == true)
+        {
+            op<<"\n"<<timer<<" : ("<<curr->get_building()->get_building_num()<<","<<timer<<")"<<endl;
+            RBT.deleteNode(curr);
+            completed_bldg_cnt ++;
+            building_finished = false;
             // clearing up variales for next instance
             execution_on = false;
             start_time = 0;
@@ -163,14 +202,6 @@ if(timer!= 0)
             target = 0;
             stop_time = 0;
         }
-        else
-        {
-            //cout<<"ongoing"<<endl;
-            //curr->get_building()->executed_time += 1;
-        }
-
-        }
-     }
      debug_op<<timer<<endl;
      timer ++;
 
